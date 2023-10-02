406dcf4ccea3eadad11449b6fbabb3a5892a5f4e
Jill Allan <54241079+jillallan@users.noreply.github.com>
2023-10-02 19:37:41 +0100

    Add JournalDataContainer and DataGeneration

--- Journal/DataGeneration.swift
+++ Journal/DataGeneration.swift
@@ -6,3 +6,54 @@
 //
 
 import Foundation
+import SwiftData
+
+class DataGeneration {
+    static func generateData(modelContext: ModelContext) {
+        modelContext.insert(Trip.bedminsterToBeijing)
+        modelContext.insert(Trip.mountains)
+        
+        modelContext.insert(Activity.templeMeads)
+        modelContext.insert(Activity.templeMeadsToPaddington)
+        modelContext.insert(Activity.paddington)
+
+        modelContext.insert(Step.templeMeadsStation)
+        modelContext.insert(Step.bathSpaStation)
+        modelContext.insert(Step.chippenhamStation)
+        modelContext.insert(Step.swindonStation)
+        modelContext.insert(Step.didcotStation)
+        modelContext.insert(Step.readingStation)
+        modelContext.insert(Step.paddingtonStation)
+        
+        modelContext.insert(Placemark.templeMeadsStation)
+        modelContext.insert(Placemark.bathSpaStation)
+        modelContext.insert(Placemark.chippenhamStation)
+        modelContext.insert(Placemark.swindonStation)
+        modelContext.insert(Placemark.didcotStation)
+        modelContext.insert(Placemark.readingStation)
+        modelContext.insert(Placemark.paddingtonStation)
+
+        Activity.templeMeads.trip = Trip.bedminsterToBeijing
+        Activity.templeMeadsToPaddington.trip = Trip.bedminsterToBeijing
+        Activity.paddington.trip = Trip.bedminsterToBeijing
+        
+        Activity.templeMeads.suceedingActivity = Activity.templeMeadsToPaddington
+        Activity.templeMeadsToPaddington.suceedingActivity = Activity.paddington
+
+        Step.templeMeadsStation.activity = Activity.templeMeads
+        Step.bathSpaStation.activity = Activity.templeMeadsToPaddington
+        Step.chippenhamStation.activity = Activity.templeMeadsToPaddington
+        Step.swindonStation.activity = Activity.templeMeadsToPaddington
+        Step.didcotStation.activity = Activity.templeMeadsToPaddington
+        Step.readingStation.activity = Activity.templeMeadsToPaddington
+        Step.paddingtonStation.activity = Activity.paddington
+        
+        Step.templeMeadsStation.placemark = Placemark.templeMeadsStation
+        Step.bathSpaStation.placemark = Placemark.bathSpaStation
+        Step.chippenhamStation.placemark = Placemark.chippenhamStation
+        Step.swindonStation.placemark = Placemark.swindonStation
+        Step.didcotStation.placemark = Placemark.didcotStation
+        Step.readingStation.placemark = Placemark.readingStation
+        Step.paddingtonStation.placemark = Placemark.paddingtonStation
+    }
+}
--- Journal/JournalDataContainer.swift
+++ Journal/JournalDataContainer.swift
@@ -5,4 +5,45 @@
 //  Created by Jill Allan on 02/10/2023.
 //
 
-import Foundation
+import SwiftData
+import SwiftUI
+
+struct JournalDataContainerViewModifier: ViewModifier {
+    let container: ModelContainer
+    
+    init(inMemory: Bool) {
+        do {
+            let configuration = ModelConfiguration(isStoredInMemoryOnly: inMemory)
+            container = try ModelContainer(for: Trip.self, configurations: configuration)
+        } catch {
+            fatalError("Unable to load model container: \(error.localizedDescription)")
+        }
+    }
+    
+    func body(content: Content) -> some View {
+        content
+            .generateData()
+            .modelContainer(container)
+    }
+}
+
+struct GenerateDataViewModifier: ViewModifier {
+    @Environment(\.modelContext) private var modelContext
+    
+    func body(content: Content) -> some View {
+        content
+            .onAppear {
+                DataGeneration.generateData(modelContext: modelContext)
+            }
+    }
+}
+
+extension View {
+    func journalDataContainer(inMemory: Bool) -> some View {
+        modifier(JournalDataContainerViewModifier(inMemory: inMemory))
+    }
+    
+    func generateData() -> some View {
+        modifier(GenerateDataViewModifier())
+    }
+}
