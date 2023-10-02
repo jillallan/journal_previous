1d9da42ab58eedd3f003b3139b31d69864caf1d4
Jill Allan <54241079+jillallan@users.noreply.github.com>
2023-10-02 19:36:35 +0100

    Add BaseTestCase

---
+++ Journal/JournalDataContainer.swift
@@ -0,0 +1,8 @@
+//
+//  JournalDataContainer.swift
+//  Journal
+//
+//  Created by Jill Allan on 02/10/2023.
+//
+
+import Foundation
---
+++ JournalTests/BaseTestCase.swift
@@ -0,0 +1,30 @@
+//
+//  JournalTests.swift
+//  JournalTests
+//
+//  Created by Jill Allan on 01/10/2023.
+//
+
+import SwiftData
+import XCTest
+@testable import Journal
+
+class BaseTestCase: XCTestCase {
+
+    override func setUpWithError() throws {
+
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
