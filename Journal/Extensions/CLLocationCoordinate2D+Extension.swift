2b2f7f534d5916c8c4b57e3265979bc3699636b2
Jill Allan <54241079+jillallan@users.noreply.github.com>
2023-10-04 19:24:49 +0100

    Remove codable conformance for CLLocationcoordinate2D

--- Journal/Extensions/CLLocationCoordinate2D+Extension.swift
+++ Journal/Extensions/CLLocationCoordinate2D+Extension.swift
@@ -8,30 +8,6 @@
 import CoreLocation
 import Foundation
 
-// Added codable conformance to enable use as a property in swift data
-extension CLLocationCoordinate2D: Codable {
-    enum CodingKeys: CodingKey {
-        case latitude
-        case longitude
-    }
-    
-    public init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.init()
-        latitude = try container.decode(Double.self, forKey: .latitude)
-        longitude = try container.decode(Double.self, forKey: .longitude)
-    }
-    
-    public func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(latitude, forKey: .latitude)
-        try container.encode(longitude, forKey: .longitude)
-    }
-    
-    static var defaultCoordinates: CLLocationCoordinate2D {
-        CLLocationCoordinate2D(latitude: 0.0, longitude: 0.0)
-    }
-}
 
 // Added equatable conformance so we can fetch steps based on their coordinates
 extension CLLocationCoordinate2D: Equatable {
@@ -39,3 +15,28 @@ extension CLLocationCoordinate2D: Equatable {
         return lhs.latitude == rhs.latitude && lhs.longitude == rhs.longitude
     }
 }
+
+// Added codable conformance to enable use as a property in swift data
+//extension CLLocationCoordinate2D: Codable {
+//    enum CodingKeys: CodingKey {
+//        case latitude
+//        case longitude
+//    }
+//
+//    public init(from decoder: Decoder) throws {
+//        let container = try decoder.container(keyedBy: CodingKeys.self)
+//        self.init()
+//        latitude = try container.decode(Double.self, forKey: .latitude)
+//        longitude = try container.decode(Double.self, forKey: .longitude)
+//    }
+//
+//    public func encode(to encoder: Encoder) throws {
+//        var container = encoder.container(keyedBy: CodingKeys.self)
+//        try container.encode(latitude, forKey: .latitude)
+//        try container.encode(longitude, forKey: .longitude)
+//    }
+//
+//    static var defaultCoordinates: CLLocationCoordinate2D {
+//        CLLocationCoordinate2D(latitude: 0.0, longitude: 0.0)
+//    }
+//}
